601020532f3c1d723986cb38a98945a1c22acf3d
satish <satti.atcha@gmail.com>
2016-10-13 15:19:28 +0530

    runlength encoding

---
+++ strings/runlengthencoding.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LEN 100
+
+char *runLengthEncoding(char *sourceStr)
+{
+	int len = strlen(sourceStr), resultLen, index, j=0, k;
+	char *finalStr = (char *)malloc(sizeof(char)*(len*2 + 1));
+	char count[MAX_LEN];
+	for(index = 0; index < len; index++)
+	{
+		finalStr[j++] = sourceStr[index];
+		resultLen = 1;
+		while( index + 1 < len && sourceStr[index] == sourceStr[index+1])
+		{
+			resultLen++;
+			index++;
+		}
+		//copy resultLen in a character array count[]
+		sprintf( count, "%d", resultLen);
+
+		//copy the count[] to finalStr
+		for(k = 0; count[k]; k++, j++)
+			finalStr[j] = count[k];
+	}
+	finalStr[j] = '\0';
+	return finalStr;
+}
+
+int main()
+{
+	char str[] = "ssssaaaatiiisshh";
+	printf("%s\n", runLengthEncoding(str));
+	return 0;
+}
