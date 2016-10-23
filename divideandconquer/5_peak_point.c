208e3072c6d106044ce188bb8895356dd598e351
satish <satti.atcha@gmail.com>
2016-10-23 15:22:53 +0530

    find peak index of array

---
+++ divideandconquer/5_peak_point.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int findPeakIndex(int *arr, int size, int start, int end)
+{
+	int middle = start + (end - start)/2;
+
+	if((middle == 0 || arr[middle - 1] <= arr[middle]) && 
+		(middle == size-1 || arr[middle+1] <= arr[middle]))
+		return middle;
+	else if(middle > 0 && arr[middle-1] > arr[middle])
+		return findPeakIndex(arr, size, start, (middle - 1));
+	return findPeakIndex(arr, size, (middle + 1), end);
+}
+
+int main()
+{
+	int *arr, size, X;
+	printf("Enter size of an array\n");
+	scanf("%d", &size);
+	//allocate memory for array
+	arr = (int *)malloc(size * sizeof(int));
+
+	printf("Enter Array elements ");
+	for(int index = 0; index < size; index++)
+		scanf("%d", &arr[index]);
+
+	printf("Peak point is = %d\n", findPeakIndex(arr, size, 0, size-1));
+	return 0;
+}
