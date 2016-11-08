a679144cad02976ae8b30a0037d6bffbf6eb6bad
satish <satti.atcha@gmail.com>
2016-11-08 19:22:06 +0530

    find middle node

--- linkedlists/SLL_FIND_MIDDLE_NODE_3.c
+++ linkedlists/SLL_FIND_MIDDLE_NODE_3.c
@@ -1,7 +1,3 @@
-/* Traverse the linked list using two pointers. Move one pointer by one step and 
-another pointer by two steps. When faster pointer moves to end of the list then
-slow pointer will reach middle of the list*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -10,11 +6,8 @@ struct node
 {
 	int data;
 	struct node *next;
-
 };
 
-/* Given a reference to the head of the list and insert a new
-node at beginning of a list*/
 void insertAtBeginning(struct node **head, int data)
 {
 	// allocate new node
@@ -26,20 +19,15 @@ void insertAtBeginning(struct node **head, int data)
 
 void findMiddleNode(struct node *head)
 {
-	/*Single pointer mid is used to find out middle node in a list
-	  'mid' pointer will move only when nodes count will 
-		become odd while traversing list */
-	 struct node *mid, *temp;
-	 int count = 0;
-	 mid = temp = head;	 
-	 while (temp != NULL) 
-	 {
+	struct node *mid = head, *temp;
+	int count = 0;
+	for(temp = head; temp ; temp = temp->next) 
+	{
 	 	if (count & 1)
 	 		mid = mid->next;
 	 	count += 1;
-	 	temp = temp->next;
-	 }
-	 if (mid != NULL)
+	}
+	if (mid)
 	 	printf("The middle node is = %d", mid->data);	
 }
 
