0ffa152ef7bcee19b320fae676998d5efc11f1ab
satish <satti.atcha@gmail.com>
2016-11-15 16:17:54 +0530

    maximum size square sub matrix

--- dynamicprograming/1_maximum_size_square_sub_matrixwith_all_1's.c
+++ dynamicprograming/1_maximum_size_square_sub_matrixwith_all_1's.c
@@ -1,75 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define ROWS 6
+#define COLS 5
 
-int minimum(int a, int b)
+int min(int a, int b)
 {
-	return a > b? a: b;
+	return a < b ? a: b;
 }
 
-int min(int a, int b, int c)
+int minimum(int a, int b , int c)
 {
-	return minimum(minimum(a, b), c);
+	return min(min(a, b), c);
 }
-
-void getMaxSubSquare(int **arr, int rows, int cols)
+ 
+void getMaximumSubSquare(int arr[ROWS][COLS])
 {
-	int row, col, maxRow, maxCol, resultMax;
+	int i, j, temp[ROWS][COLS], resultMax, maxOfRow, maxOfCol;
+	for(i = 0; i < ROWS; i++)
+		temp[i][0] = arr[i][0];
 
-	int **temp = (int **)malloc(sizeof(int *) * rows);
-    for(col = 0 ; col < cols; col++)
-        temp[col] = (int *)malloc(sizeof(int) * rows);
+	for(j = 0; j < COLS; j++)
+		temp[0][j] = arr[0][j];
 
-	for(row = 0; row < rows; row++)
-		temp[row][0] = arr[row][0];
-
-	for(col = 0; col < cols; col++)
-		temp[0][col] = arr[0][col];
-
-	for(row = 1; row < rows; row++)
-		for(col = 1; col < cols; col++)
-			temp[row][col] = (arr[row][col] == 1)? 
-			min(temp[row][col-1], temp[row - 1][col], temp[row - 1][col - 1]) + 1: 0;
-
-	// resultMax = temp[0][0]; maxRow = 0; maxCol = 0;
-	// for(row = 0; row < rows; row++)
-	// {
-	// 	for(col = 0; col < cols; col++)
-	// 	{
-	// 		if(resultMax < temp[row][col])
-	// 		{
-	// 			resultMax = temp[row][col];
-	// 			maxRow = row; maxCol = col;
-	// 		}
-	// 	}
-	// }
-	// printf("\n Maximum size sub-matrix with 1's: \n");
-	// for(row = maxRow; row > maxRow - resultMax; row--)
-	// {
-	// 	for(col = maxCol; col > maxCol - resultMax; col--)
-	// 		printf("%d ", arr[row][col]);
-	// 	printf("\n");
-	// }
-}
-
+	for(i = 1; i < ROWS; i++)
+	{
+		for(j = 1; j < COLS; j++)
+		{
+			if(arr[i][j] == 1)
+				temp[i][j] = minimum(temp[i][j-1], temp[i-1][j], temp[i-1][j-1]) + 1;
+			else
+				temp[i][j] = 0;
+		}
+	}
+	resultMax = temp[0][0], maxOfRow = 0, maxOfCol = 0;
+	for(i = 0; i < ROWS; i++)
+	{
+		for(j = 0; j < COLS; j++)
+		{
+			if(resultMax < temp[i][j])
+			{
+				resultMax = temp[i][j];
+				maxOfRow = i;
+				maxOfCol = j;
+			}
+		}
+	}
+	printf(" Maximum size sub-matrix is: \n");
+	for( i = maxOfRow; i > maxOfRow - resultMax; i--)
+	{
+		for(j = maxOfCol; j > maxOfCol - resultMax; j--)
+		{
+			printf("%d\t", arr[i][j]);
+		}
+		printf("\n");
+	}
+}     
+      
+ 
 int main()
 {
-	int rows, cols, i, j;
-	printf("Enter number of rows in matrix\n");
-	scanf("%d", &rows);
+	int arr[ROWS][COLS] =  {{0, 1, 1, 0, 1}, 
+                   {1, 1, 0, 1, 0}, 
+                   {0, 1, 1, 1, 0},
+                   {1, 1, 1, 1, 0},
+                   {1, 1, 1, 1, 1},
+                   {0, 0, 0, 0, 0}};
+    getMaximumSubSquare(arr);
+    return 0;                
+} 
 
-	printf("Enter number of columns in matrix\n");
-	scanf("%d", &cols);
-
-	//allocate memory
-	int **arr = (int **)malloc(sizeof(int *) * rows);
-    for(i = 0 ; i < cols; i++)
-        arr[i] = (int *)malloc(sizeof(int) * rows);
-
-    printf("Enter elements in matrix\n");
-    for(i = 0; i < rows; i++)
-    	for(j = 0; j < cols; j++)
-    		scanf("%d", &arr[i][j]);
-    getMaxSubSquare(arr, rows, cols);
-    return 0;
-
-}
+//Time complexity: O(m * n)
+//Space complexity: O(m * n)
